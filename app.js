344474e91ea4816b02db6dce07ec5bb9f5177d8c
Mursel <mursal.haxverdiyev@gmail.com>
2025-07-08 21:11:08 +0400

    title added

--- app.js
+++ app.js
@@ -29,8 +29,8 @@ function formatVideoTitle(title) {
     safeTitle = "video-" + Date.now();
   }
 
-  if (safeTitle.length > 100) {
-    safeTitle = safeTitle.substring(0, 100);
+  if (safeTitle.length > 30) {
+    safeTitle = safeTitle.substring(0, 30);
   }
 
   return safeTitle;
@@ -43,7 +43,7 @@ function getTitle(url) {
 
     exec(command, (error, stdout) => {
       if (error) {
-        console.error("Error fetching title:", error);
+        console.log("Error fetching title:", error);
         return reject(error);
       }
       resolve(stdout.trim());
@@ -53,19 +53,38 @@ function getTitle(url) {
 
 app.use("/download", express.static(path.join(__dirname, "download")));
 
-app.post("/download-video", async (req, res) => {
+app.get("/test", async (req, res) => {
+  let safeTitle = "";
+  try {
+    let rawTitle = await getTitle(
+      "https://youtu.be/CH50zuS8DD0?si=bdde62F5g57OcEzi"
+    );
+    safeTitle = formatVideoTitle(rawTitle);
+  } catch (error) {
+    console.log("Error testing:", error);
+    return res.status(500).json({ success: false, message: "Error testing" });
+  }
+  if (safeTitle == "1-Minute-Timer") {
+    res.status(200).json({
+      success: true,
+      message: "Server is working",
+    });
+  } else {
+    res.status(500).json({
+      success: false,
+      message: "Server is not working",
+    });
+  }
+});
+
+app.post("/get-video-title", async (req, res) => {
   try {
     const videoUrl = req.body.url;
-    const videoQuality = req.body.quality || "1080";
-    const audioOnly = req.body.audio || false;
-    const host = req.get("host");
-
     if (!videoUrl) {
       return res
         .status(400)
         .json({ success: false, message: "Video URL is required" });
     }
-
     let rawTitle;
     try {
       rawTitle = await getTitle(videoUrl);
@@ -75,15 +94,63 @@ app.post("/download-video", async (req, res) => {
         message: "Failed to retrieve video title",
       });
     }
-
     const safeTitle = formatVideoTitle(rawTitle);
-    const outputPath = `download/${safeTitle}.${audioOnly ? "mp3" : "mp4"}`;
+    res.status(200).json({
+      success: true,
+      data: {
+        title: safeTitle,
+      },
+    });
+  } catch (error) {
+    console.log("Server error:", error);
+    res.status(500).json({
+      success: false,
+      message: "Server error",
+    });
+  }
+});
+
+app.post("/download-video", async (req, res) => {
+  try {
+    const videoUrl = req.body.url;
+    const videoQuality = req.body.quality || "1080";
+    const audioOnly = req.body.audio || false;
+    let videoTitle = req.body.title || "";
+    const host = req.get("host");
+
+    if (!videoUrl) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Video URL is required" });
+    }
+
+    if (["360", "480", "720", "1080"].indexOf(videoQuality) === -1) {
+      return res.status(400).json({
+        success: false,
+        message:
+          "Invalid video quality. Allowed values are 360, 480, 720, 1080",
+      });
+    }
+
+    if (!videoTitle) {
+      let rawTitle;
+      try {
+        rawTitle = await getTitle(videoUrl);
+      } catch (error) {
+        return res.status(400).json({
+          success: false,
+          message: "Failed to retrieve video title",
+        });
+      }
+      videoTitle = formatVideoTitle(rawTitle);
+    }
+    const outputPath = `download/${videoTitle}.${audioOnly ? "mp3" : "mp4"}`;
     const cookiesFile = path.join(__dirname, "cookies.txt");
 
     let command = "";
 
     if (audioOnly) {
-      command = `yt-dlp --cookies "${cookiesFile}" -o "download/${safeTitle}.%(ext)s" -x --audio-format mp3 --audio-quality 0 "${videoUrl}"`;
+      command = `yt-dlp --cookies "${cookiesFile}" -o "download/${videoTitle}.%(ext)s" -x --audio-format mp3 --audio-quality 0 "${videoUrl}"`;
     } else {
       const formatSelector = `bestvideo[height<=${videoQuality}][ext=mp4]+bestaudio[ext=m4a]/best[height<=${videoQuality}]/best`;
       command = `yt-dlp --cookies "${cookiesFile}" -o "${outputPath}" -f "${formatSelector}" "${videoUrl}"`;
@@ -93,15 +160,15 @@ app.post("/download-video", async (req, res) => {
       res.status(200).json({
         success: true,
         data: {
-          url: `https://${host}/download/${safeTitle}.${
+          url: `https://${host}/download/${videoTitle}.${
             audioOnly ? "mp3" : "mp4"
           }`,
-          name: safeTitle,
+          name: videoTitle,
         },
       });
     });
   } catch (error) {
-    console.error("Server error:", error);
+    console.log("Server error:", error);
     res.status(500).json({
       success: false,
       message: "Server error",
